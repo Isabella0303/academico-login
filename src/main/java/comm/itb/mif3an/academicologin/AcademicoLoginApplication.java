22a5ddde9d9373ddbb2afbff296ee82526919e01
Isabella Ferreira <rm89823@estudante.fieb.edu.br.gmail.com>
2023-07-31 20:38:49 -0300

    Inicio contrele de versão

---
+++ src/main/java/comm/itb/mif3an/academicologin/AcademicoLoginApplication.java
@@ -0,0 +1,13 @@
+package comm.itb.mif3an.academicologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoLoginApplication.class, args);
+	}
+
+}
